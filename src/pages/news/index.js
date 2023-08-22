7e1d3064303e9c8a6e703fa170b7b45812e8a233
arifmia1129 <arifmia1129@gmail.com>
2023-08-22 08:33:40 +0600

    make rootlayout component and wrap news page

---
+++ src/pages/news/index.js
@@ -0,0 +1,48 @@
+import RootLayout from "@/components/Layouts/RootLayout";
+import { Card, Col, Row } from "antd";
+import Head from "next/head";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+export default function News() {
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    fetch("./data.json")
+      .then((res) => res.json())
+      .then((data) => setNews(data));
+  }, []);
+  return (
+    <>
+      <Head>
+        <title>News</title>
+        <meta name="news" description="All news of fast news" />
+      </Head>
+      <Row gutter={16}>
+        {news?.map((item) => (
+          <Col key={item.id} span={8}>
+            <Card style={{ height: 600, marginBottom: 2 }} bordered={false}>
+              <Image
+                src={item.imageUrl}
+                width={50}
+                height={50}
+                layout="responsive"
+                alt="news-image"
+              />
+              <h1>{item.title}</h1>
+              <div>
+                <p style={{ color: "gray" }}>{item.reporterName}</p>
+                <p style={{ color: "gray", marginTop: -10 }}>
+                  {item.publicDate}
+                </p>
+              </div>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+}
+
+News.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
